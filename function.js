0c4d5e30b9d24dde64dd67e81ac8ab1d1b34568d
imros10 <imros.arefen@gmail.com>
2022-06-27 17:11:10 +0600

    90%

--- function.js
+++ function.js
@@ -1,24 +1,42 @@
+    // user pints 
+        let userPoint = 0;
+        let computerPoint = 0;
+        const range = document.getElementById('get-value');
+        console.log(range.value)
     document.getElementById('top-button-handle').addEventListener('click',function(event){
-
+        
+            
         // user input 
-        const userInput = event.target.value;
-        // computer function 
+        const userInput =event.target.value;
+        if(userInput){
+             // computer function 
         const computerMemory = ['rock','paper','scissors'];
         const computerSay = computerMemory[Math.floor(Math.random() * computerMemory.length)];
         // point declare 
 
         // start condition 
-        mainFunction(userInput,computerSay)
+        mainFunction(userInput,computerSay,1)
         // console.log(computerSay);
+        }else{
+            document.body.innerHTML = `<div class="text-center mt-5">
+            <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>`;
+        }
+       
         
     })
 
 
     // main function 
-    function mainFunction(userInput,computerSay){
+    function mainFunction(userInput,computerSay,power){
+        // User point dom 
+        const userDom = document.getElementById('your-main')
+        const computerDom = document.getElementById('computer-main')
         // user pints 
-        let userPoint = 0;
-        let computerPoint = 0;
+        // let userPoint = 0;
+        // let computerPoint = 0;
         
         const userImage = document.getElementById('user-image');
         const computerImage = document.getElementById('computer-image');
@@ -41,16 +59,77 @@
             
         }
         else if(userInput == 'rock' && computerSay == 'paper'){
+            computerPoint = computerPoint + 1;
+            console.log(computerPoint)
+            computerDom.innerText=computerPoint
+
             userPlayd.innerText=`You played ${userInput}`
             ComputerPlayd.innerText=`Computer played ${computerSay}`
             resultArea.innerText="Computer Win"
-            if((userInput && computerSay) == 'rock'){
+            
                 console.log(userInput)
                 userImage.setAttribute('src', `image/left ${userInput}.png`)
-                computerImage.setAttribute('src', `image/left ${userInput}.png`)   
-            }else{
-                userImage.setAttribute('src', `image/left ${userInput}.png`)
-            computerImage.setAttribute('src', `image/right ${userInput}.png`)
+            computerImage.setAttribute('src', `image/right ${computerSay}.png`)
             }
-        }
+        else if(userInput == 'rock' && computerSay == 'scissors'){
+            userPoint = userPoint + 1;
+            userDom.innerText=userPoint
+
+            userPlayd.innerText=`You played ${userInput}`
+            ComputerPlayd.innerText=`Computer played ${computerSay}`
+            resultArea.innerText="You Win"
+            
+                console.log(userInput)
+                userImage.setAttribute('src', `image/left ${userInput}.png`)
+            computerImage.setAttribute('src', `image/right ${computerSay}.png`)
+            }
+        else if(userInput == 'paper' && computerSay == 'rock'){
+            userPoint = userPoint + 1;
+            userDom.innerText=userPoint
+
+            userPlayd.innerText=`You played ${userInput}`
+            ComputerPlayd.innerText=`Computer played ${computerSay}`
+            resultArea.innerText="You Win"
+            
+                console.log(userInput)
+                userImage.setAttribute('src', `image/left ${userInput}.png`)
+            computerImage.setAttribute('src', `image/right ${computerSay}.png`)
+            }
+        else if(userInput == 'paper' && computerSay == 'scissors'){
+            computerPoint = computerPoint + 1;
+            computerDom.innerText=computerPoint
+
+            userPlayd.innerText=`You played ${userInput}`
+            ComputerPlayd.innerText=`Computer played ${computerSay}`
+            resultArea.innerText="Computer Win"
+            
+                console.log(userInput)
+                userImage.setAttribute('src', `image/left ${userInput}.png`)
+            computerImage.setAttribute('src', `image/right ${computerSay}.png`)
+            }
+        else if(userInput == 'scissors' && computerSay == 'rock'){
+            computerPoint = computerPoint + 1;
+            computerDom.innerText=computerPoint
+
+            userPlayd.innerText=`You played ${userInput}`
+            ComputerPlayd.innerText=`Computer played ${computerSay}`
+            resultArea.innerText="Computer Win"
+            
+                console.log(userInput)
+                userImage.setAttribute('src', `image/left ${userInput}.png`)
+            computerImage.setAttribute('src', `image/right ${computerSay}.png`)
+            }
+        else if(userInput == 'scissors' && computerSay == 'paper'){
+            userPoint = userPoint + 1;
+            userDom.innerText=userPoint
+
+            userPlayd.innerText=`You played ${userInput}`
+            ComputerPlayd.innerText=`Computer played ${computerSay}`
+            resultArea.innerText="You win"
+            
+                console.log(userInput)
+                userImage.setAttribute('src', `image/left ${userInput}.png`)
+            computerImage.setAttribute('src', `image/right ${computerSay}.png`)
+            }
+        
     }
